da5b861b7b4c50e32109f78ae9f04fa9df2be60e
Johannes Schatteiner <johannes.schatteiner@gmx.at>
2026-05-07 10:36:49 +0200

    Fix: Bump version code to 3 to reflect the version bump to v1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "at.jschatteiner.enitemhdtranslator"
         minSdk = 23
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
     }
 
